22e1a61ecd7e845dc4d472a39e31864780d4cb4d
Guilherme <guilhermesm9@al.insper.edu.br>
2021-11-17 08:42:09 -0300

    adicionando sprites

--- jogov1.py
+++ jogov1.py
@@ -8,7 +8,10 @@ display.set_caption('Taiko no Chi')
 game= True
 
 sprites = {}
-sprites['bolinha'] = pygame.image.load('sprites/input_b.png')
+sprites['bolinha1'] = pygame.image.load('sprites/input_b.png')
+sprites['bolinha2'] = pygame.image.load('sprites/input_r.png')
+sprites['bolinha3'] = pygame.image.load('sprites/input_judge.png')
+sprites['linha'] = pygame.image.load('sprites/move_line.png')
 
 while game:
     for event in pygame.event.get():
